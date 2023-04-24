0de42f93cb334b57d7decc2672fc65e1f48bf040
Sacha Weatherstone <sachaw100@hotmail.com>
2023-04-24 23:39:45 +1000

    Add rome and format

--- bin/clean.ts
+++ bin/clean.ts
@@ -5,10 +5,10 @@ import { variants } from "./generate.js";
 const iconSRCPath = "src/icons";
 
 variants.map((variant) => {
-	const files = readdirSync(join(process.cwd(), iconSRCPath, variant));
-	files.map((file) => {
-		if (file.endsWith(".tsx") || file.endsWith(".ts")) {
-			unlinkSync(join(process.cwd(), join(iconSRCPath, variant, file)));
-		}
-	});
+  const files = readdirSync(join(process.cwd(), iconSRCPath, variant));
+  files.map((file) => {
+    if (file.endsWith(".tsx") || file.endsWith(".ts")) {
+      unlinkSync(join(process.cwd(), join(iconSRCPath, variant, file)));
+    }
+  });
 });
--- bin/generate.ts
+++ bin/generate.ts
@@ -7,27 +7,27 @@ export const variants = ["bold", "duotone", "fill", "light", "regular", "thin"];
 const iconSRCPath = "src/icons";
 
 variants.map((variant) => {
-	icons.map((icon) => {
-		const iconPath = `node_modules/@phosphor-icons/core/assets/${variant}/${
-			icon.name
-		}${variant !== "regular" ? `-${variant}` : ""}.svg`;
+  icons.map((icon) => {
+    const iconPath = `node_modules/@phosphor-icons/core/assets/${variant}/${
+      icon.name
+    }${variant !== "regular" ? `-${variant}` : ""}.svg`;
 
-		const exists = existsSync(join(process.cwd(), iconPath));
+    const exists = existsSync(join(process.cwd(), iconPath));
 
-		if (!exists) {
-			return console.log(
-				`Could not find icon ${icon.name} of variant ${variant}.`,
-			);
-		}
+    if (!exists) {
+      return console.log(
+        `Could not find icon ${icon.name} of variant ${variant}.`,
+      );
+    }
 
-		const data = readFileSync(join(process.cwd(), iconPath), "utf8");
+    const data = readFileSync(join(process.cwd(), iconPath), "utf8");
 
-		const svg = data.substring(
-			data.indexOf("><") + 1,
-			data.lastIndexOf("><") + 1,
-		);
+    const svg = data.substring(
+      data.indexOf("><") + 1,
+      data.lastIndexOf("><") + 1,
+    );
 
-		const template = `//Generated file, do not edit
+    const template = `//Generated file, do not edit
 import { Component, JSX } from "solid-js";
 
 export const ${icon.pascal_name}: Component<JSX.SvgSVGAttributes<SVGSVGElement>> = (props) => {
@@ -45,44 +45,44 @@ export const ${icon.pascal_name}: Component<JSX.SvgSVGAttributes<SVGSVGElement>>
 	);
 };`;
 
-		writeFile(
-			join(process.cwd(), `${iconSRCPath}/${variant}/${icon.pascal_name}.tsx`),
-			template,
-			(err) => {
-				if (err) {
-					return console.log(
-						`Could not write icon ${icon.name} of variant ${variant}.`,
-					);
-				}
-			},
-		);
-	});
+    writeFile(
+      join(process.cwd(), `${iconSRCPath}/${variant}/${icon.pascal_name}.tsx`),
+      template,
+      (err) => {
+        if (err) {
+          return console.log(
+            `Could not write icon ${icon.name} of variant ${variant}.`,
+          );
+        }
+      },
+    );
+  });
 });
 
 variants.forEach((variant) => {
-	const variantIndexFile = `// Generated file, do not edit
+  const variantIndexFile = `// Generated file, do not edit
 ${icons
-	.map((icon) => {
-		const iconPath = join(
-			process.cwd(),
-			`src/icons/${variant}/${icon.pascal_name}.tsx`,
-		);
-		const exists = existsSync(iconPath);
-		return exists
-			? `export { ${icon.pascal_name} as ${icon.pascal_name}Icon, ${icon.pascal_name} } from "./${icon.pascal_name}.jsx";`
-			: "";
-	})
-	.join("\n")}
+  .map((icon) => {
+    const iconPath = join(
+      process.cwd(),
+      `src/icons/${variant}/${icon.pascal_name}.tsx`,
+    );
+    const exists = existsSync(iconPath);
+    return exists
+      ? `export { ${icon.pascal_name} as ${icon.pascal_name}Icon, ${icon.pascal_name} } from "./${icon.pascal_name}.jsx";`
+      : "";
+  })
+  .join("\n")}
 `;
 
-	writeFile(
-		join(process.cwd(), `src/icons/${variant}/index.ts`),
-		variantIndexFile,
-		(err) => {
-			if (err) {
-				return console.log(`Could not write index.ts for ${variant}`);
-			}
-			console.log(`index.ts file for ${variant} created successfully.`);
-		},
-	);
+  writeFile(
+    join(process.cwd(), `src/icons/${variant}/index.ts`),
+    variantIndexFile,
+    (err) => {
+      if (err) {
+        return console.log(`Could not write index.ts for ${variant}`);
+      }
+      console.log(`index.ts file for ${variant} created successfully.`);
+    },
+  );
 });
