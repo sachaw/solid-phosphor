08c872970702af81b0e31db4ecf0a22d241ce18d
Sacha Weatherstone <sachaw100@hotmail.com>
2024-01-15 19:58:03 +1000

    bump deps

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import { JSX } from "solid-js/jsx-runtime";
 import * as BoldIcons from "./icons/bold/index.js";
 import * as DuotoneIcons from "./icons/duotone/index.js";
 import * as FillIcons from "./icons/fill/index.js";
 import * as LightIcons from "./icons/light/index.js";
 import * as RegularIcons from "./icons/regular/index.js";
 import * as ThinIcons from "./icons/thin/index.js";
-import { Component } from "solid-js";
+import type { Component, JSX } from "solid-js";
 
 type PhosphorIconProps = JSX.SvgSVGAttributes<SVGSVGElement>;
 type PhosphorIcon = Component<PhosphorIconProps>;
@@ -17,6 +16,9 @@ export {
   LightIcons,
   RegularIcons,
   ThinIcons,
+};
+
+export type {
   PhosphorIconProps,
   PhosphorIcon,
-};
+}
